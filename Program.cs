77f817c85693b33e1b2fc7eda696136c6460f0d1
Ak2raProg <ak2ra.prog@gmail.com>
2022-12-09 22:45:06 -0300

    Realizado Desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
-
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("--Testando dispositivos que herdam a classe Smartphone-- \n");
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "552231", modelo: "Modelo teste1", imei: "5463728203847", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("DIO");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "4657389", modelo: "Modelo teste2", imei: "7583049574632", memoria: 32);
+iphone.Ligar();
+iphone.InstalarAplicativo("Clans of Clash");
